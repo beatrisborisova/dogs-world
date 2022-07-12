ed5919397bd6faf7fbe6e3e7ed0e86b7500a779b
Beatris <beti16b@gmail.com>
2022-07-12 21:51:27 +0300

    Navigation and Home changes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,22 +1,25 @@
 import { getDogImage } from '../../services/dogs';
 import { useEffect, useState } from 'react';
 import './Home.css';
+import { NavLink } from 'react-router-dom';
 
 export const Home = () => {
 
     const [dogImg, setDogImg] = useState('');
 
     useEffect(() => {
-        getDogImage()
-            .then(img => setDogImg(img))
-    }, [])
+        setTimeout(() => {
+            getDogImage()
+                .then(img => setDogImg(img))
+        }, 3000)
+    }, [dogImg])
 
     return (
         <div className="home-container" style={{ backgroundImage: `url(${dogImg})` }}>
-            <div  className='home-content'>
+            <div className='home-content'>
                 Welcome to the DOGS World
                 {/* Линк към страницата, от която се избира към осиновяване или към купуване страницата */}
-                <a href="#">Exlore</a>
+                <NavLink to={'catalog'} className="btn-level-one">EXPLORE</NavLink>
             </div>
         </div>
     )
--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,26 +1,34 @@
 import './Common.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
+
     return (
         <header>
             <div className="image-wrapper-logo">
-                LOGO HERE
+                <NavLink to={'/'}>LOGO HERE</NavLink>
             </div>
-            <nav>
+            <nav className='nav'>
                 <ul className="navigation-list">
                     <NavLink to={'/'}>Home</NavLink>
-                    <NavLink to={'/catalog'}>Име</NavLink>
+                    <NavLink to={'/catalog'} className='dropdown'>
+                        <button class="dropbtn">Main</button>
+                        <div class="dropdown-container">
+                            <NavLink to={'/catalog/adopt'}>Adopt</NavLink>
+                            <NavLink to={'/catalog/buy'}>Buy</NavLink>
+                        </div>
+                    </NavLink>
                     <NavLink to={'/'}>Cause</NavLink>
                     <NavLink to={'/'}>Contacts</NavLink>
                 </ul>
             </nav>
             <div>
                 <ul className="user-navigation-list">
-                    <li>Login</li>
-                    <li>Register</li>
-                    <li>Profile</li>
-                    <li>Logout</li>
+                    <NavLink to={'/login'}>Login</NavLink>
+                    <NavLink to={'/register'}>Register</NavLink>
+                    <NavLink to={'/profile'}>Profile</NavLink>
+                    <NavLink to={'/logout'}>Logout</NavLink>
                 </ul>
             </div>
         </header>
