3d5446c26928e13be3244751c689ec39f8db25ba
Beatris <beti16b@gmail.com>
2022-08-02 16:57:54 +0100

    Not fount page styled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer } from './components/common/Footer';
 import { Header } from './components/common/Header';
-import { NotFound } from './components/not-found/NotFound';
 import { AnimatedRoutes } from './components/others/AnimatedRoutes';
 import { DogContextRoutes } from './components/others/DogContextRoutes';
 
--- src/components/not-found/NotFound.js
+++ src/components/not-found/NotFound.js
@@ -1,7 +1,18 @@
-import './NotFound.css';
+import styles from './NotFound.module.css';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div>Not found Page</div>
+        <div className={styles.notFoundContainer}>
+            <div className={styles.notFoundContent}>
+                <h1>404</h1>
+                <h2>Page not found</h2>
+                <p className={styles.text}>The page you are looking for does not exist</p>
+            </div>
+            <button onClick={() => navigate('/')} className='btn-level-one'>Go to home</button>
+        </div>
     )
 }
